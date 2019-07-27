a74dc8624ccb3215c2e9c258c9e4ecb7bde0d303
willi123yao <willi123yao@gmail.com>
2019-07-27 18:20:38 +0800

    Add utils file for use in signer

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+const crypto = require('crypto');
+
+const hashlib = {
+  sha1: (data) => crypto.createHash('sha1').update(data).digest()
+}
+
+class HMACAlgorithm  {
+  constructor (digest_method) {
+    if (!digest_method) {
+      throw Error('Missing digest method!');
+    }
+    this.digest_method = digest_method;
+  }
+
+  get_signature (key, value) {
+    return crypto.createHmac(this.digest_method, key).update(value).digest();
+  }
+
+  verify_signature (key, value, sig) {
+    return crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(this.get_signature(key, value)));
+  }
+}
+
+class NoneAlgorithm {
+  get_signature (digest_method, key, value) {
+    return value;
+  }
+}
+
+const b64encode = (data) => {
+  const enc = Buffer.from(data).toString('base64');
+  return enc.split('/').join('_').split('+').join('-').split('=').join('');
+}
+
+const b64decode = (data) => {
+  data += '='.repeat(mod(-data%4 ,4));
+  return Buffer.from(data, 'base64');
+}
+
+const mod = (n, m) => {
+  return ((n * m) + m) % m;
+}
+
+module.exports = {
+  HMACAlgorithm,
+  NoneAlgorithm,
+  hashlib,
+  b64encode,
+  b64decode
+}
