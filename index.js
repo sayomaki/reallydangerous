6af28aea6d01c4617f16c1d2c4b4861b8b1ac8a6
willi123yao <willi123yao@gmail.com>
2020-01-10 16:57:48 +0800

    Include and export the Base64 module

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const Signer = require('./src/signer');
 const TimestampSigner = require('./src/timed');
 
+const Base64 = require('./src/base64');
+
 module.exports = {
   Signer,
-  TimestampSigner
+  TimestampSigner,
+  Base64
 }
